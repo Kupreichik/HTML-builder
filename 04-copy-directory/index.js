17f5487dc5fdfc366dac5301bf1669ad47a05068
Julia Kupreichik <julilike@yandex.ru>
2022-11-03 17:49:04 +0300

    feat: solve 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,49 @@
+const fs = require('fs');
+const path = require('path');
 
+let newDirPath = path.join(__dirname, 'files-copy');
+let dirPath = path.join(__dirname, 'files');
+
+function copyFiles(dirPath, newDirPath) {
+  fs.readdir(dirPath, (err, files) => {
+    
+    files.forEach(file => {
+      fs.stat(path.join(dirPath, file), (_, stats) => {
+        if (stats.isDirectory()) {
+          copyDir(
+            path.join(dirPath, file),
+            path.join(newDirPath, file));
+        } else {
+          fs.copyFile(
+            path.join(dirPath, file),
+            path.join(newDirPath, file),
+            (err) => {
+              if (err) console.log(err.message);
+            }
+          )
+        }
+      })
+    });
+
+    if (err) {
+      console.log(err.message);
+    } else {
+      console.log(`Data from ${dirPath} copied successfully\n`);
+    }
+  })
+}
+
+function copyDir(dirPath, newDirPath) {
+  fs.rm(newDirPath, { recursive: true, force: true }, (err) => {
+    if (err) console.log(err.message);
+    fs.mkdir(newDirPath, { recursive: true }, (err) => {
+      if (err) {
+        console.log(err.message);
+      } else {
+        copyFiles(dirPath, newDirPath);
+      }
+    });
+  });
+};
+
+copyDir(dirPath, newDirPath);
